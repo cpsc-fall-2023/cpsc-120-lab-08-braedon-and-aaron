b165d831f71ffc33aa6a55a3997c6073cad21b7d
Collett <braedoncollett@cs101u-2a.acad.fullerton.edu>
2023-10-25 15:52:29 -0700

    try 12

--- part-2/average.cc
+++ part-2/average.cc
@@ -10,19 +10,19 @@
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments{argv, argv + argc};
 
-  if (size(arguments) < 1) {
+  if (size(arguments) < 2) {
     std::cout << "error\n";
     return 1;
   }
 
   float sum_num = 0;
 
-  for (std::string const& argument : arguments) {
+  for (std::string& argument : arguments) {
     if (argument != "./average") {
-      sum_num += std::stof(argument);
+      sum_num = sum_num + std::stof(argument);
     }
   }
-  float average = sum_num / static_cast<float>(size(arguments) - 1);
+  float average = sum_num / static_cast<float>(arguments.size() - 1);
 
   std::cout << "average = " << average << "\n";
 
