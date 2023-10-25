9f34623ba90c25bc716cc3b5de4cb0a8aa6630d1
Villicana <fulaaron@cs101u-2b.acad.fullerton.edu>
2023-10-25 14:48:18 -0700

    run

--- part-1/sandwich.cc
+++ part-1/sandwich.cc
@@ -1,4 +1,7 @@
-// TODO: Add the required header
+// Aaron Villicana
+// FulAaron@csu.fullerton.edu
+// @Aaron-12a
+// Partners: @pythonrogrammer3
 
 #include <iostream>
 #include <string>
@@ -7,17 +10,17 @@
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments{argv, argv + argc};
 
-  // TODO: Validate that the number of arguments is correct.
-  // If not, print an error message and return a non-zero value.
+  if (size(arguments) < 3) {
+    std::cout << "error";
+    return 1;
+  }
 
-  // TODO: Declare three std::string variables to hold the
-  // protein, bread, and condiment input.
-  // Initialize each variable with an element of the arguments vector
-  // declared above.
+  std::string carbs = arguments.at(1);
+  std::string loaf = arguments.at(2);
+  std::string sauce = arguments.at(3);
 
-  // TODO: Use cout to print output following the pattern
-  // Your order: A *PROTEIN* sandwich on *BREAD* with *CONDIMENT*.
-  // on its own line.
+  std::cout << "Your order: A " << carbs << " sandwich on " << loaf << " with "
+            << sauce << ".\n";
 
   return 0;
 }
